8dd90b9bc4842539ae9760f1335d61046ab5ed71
socrathematics <65318685+socrathematics@users.noreply.github.com>
2020-05-24 14:55:15 -0700

    Add files via upload

---
+++ request-course/courseReq.js
@@ -0,0 +1,53 @@
+'use strict';
+
+//grab a form
+const form = document.querySelector('#CRForm');
+
+//grab an input
+const inputCourse = document.querySelector('#inputCourse');
+const inputWhy = document.querySelector('#inputWhy');
+console.log(form);
+console.log("got the components")
+
+//config your firebase push
+const config = {
+    apiKey: "AIzaSyA6bA3PlHiFGHB1CIWobuBUsEa9IQ7AL3I",
+    authDomain: "socrathematics.firebaseapp.com",
+    databaseURL: "https://socrathematics.firebaseio.com",
+    projectId: "socrathematics",
+    storageBucket: "socrathematics.appspot.com",
+    messagingSenderId: "809638401187",
+    appId: "1:809638401187:web:9608adf73f25ced6d45b16",
+    measurementId: "G-2QEQZPCEZR"
+};
+
+
+//create a functions to push
+    function firebasePush(course,why) {
+
+
+        //prevents from braking
+        if (!firebase.apps.length) {
+            firebase.initializeApp(config);
+        }
+
+        //push itself
+        var mailsRef = firebase.database().ref('courseRequests').push().set(
+            {
+                Course: course.value,
+                What: why.value
+            }
+        );
+
+    }
+
+//push on form submit
+    if (form) {
+        form.addEventListener('submit', function (evt) {
+            evt.preventDefault();
+            firebasePush(inputCourse, inputWhy);
+
+            //shows alert if everything went well.
+            return alert('Your Course Request has been submitted.');
+        })
+    }
