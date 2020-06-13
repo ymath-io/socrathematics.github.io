49ed80d92b648b25ce5e74d1f1d1b59d370f81cb
socrathematics <65318685+socrathematics@users.noreply.github.com>
2020-06-13 15:40:06 -0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -12,16 +12,16 @@ if (!document.getElementById("navigation")){document.write(`<div class="containe
     <a class="navbar-brand" href="/"><img src="https://socrathematics.github.io/favicon.png" height=40 width="40"> <span class="sr-only">(current)</span></a>
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item">
-        <a class="nav-link" href="/modular-arithmetic" id="mod">Modular Arithmetic</a>
+        <a class="nav-link pop" href="/modular-arithmetic" id="mod">Modular Arithmetic</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/trigonometry" id="trig">Trigonometry</a>
+        <a class="nav-link pop" href="/trigonometry" id="trig">Trigonometry</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/calculus" id="calc">Calculus</a>
+        <a class="nav-link pop" href="/calculus" id="calc">Calculus</a>
       </li>
       <li class="nav-item" >
-        <a class="nav-link" href="/request-course" id="rc">Request a Course</a>
+        <a class="nav-link pop" href="/request-course" id="rc">Request a Course</a>
       </li>
     </ul>
   </div>
