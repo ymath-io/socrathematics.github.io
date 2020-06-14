96f6fecf87f08dcce66067c9ce4aa6624c086dea
socrathematics <65318685+socrathematics@users.noreply.github.com>
2020-06-13 21:35:00 -0700

    Add files via upload

---
+++ calculus/limits/practice/check.js
@@ -0,0 +1,42 @@
+  //number one
+  var answerSpan = document.getElementById('answer1');
+  var answerMathField = MQ.MathField(answerSpan, {
+    handlers: {
+      edit: function() {
+        var enteredMath = answerMathField.latex(); // Get entered math in LaTeX format
+        checka(enteredMath,"1","1",null);
+      }
+    }
+  });
+
+    //number two
+      var answerSpan2 = document.getElementById('answer2');
+  var answerMathField2 = MQ.MathField(answerSpan2, {
+    handlers: {
+      edit: function() {
+        var enteredMath2 = answerMathField2.latex(); // Get entered math in LaTeX format
+        checka(enteredMath2,"2","4",null);
+      }
+    }
+  });
+    //number three
+      var answerSpan3 = document.getElementById('answer3');
+  var answerMathField3 = MQ.MathField(answerSpan3, {
+    handlers: {
+      edit: function() {
+        var enteredMath3 = answerMathField3.latex(); // Get entered math in LaTeX format
+        checka(enteredMath3,"3","undefined","tldne");
+      }
+    }
+  });
+
+//to check the answers!
+    function checka(l,n, a1,a2){
+    if (l==a1 || l==a2){
+    document.getElementById("answer"+n).style.color="var(--success)";
+    document.getElementById("tu"+n).style.display="inline";
+    }
+else{document.getElementById("answer"+n).style.color="var(--dark)";
+    document.getElementById("tu"+n).style.display="none";
+    }
+    }
