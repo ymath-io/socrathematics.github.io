9d842b8b6272433f85f87050126fb3308fbc745e
saumyasinghal747 <65318685+saumyasinghal747@users.noreply.github.com>
2020-07-16 17:22:52 -0700

    Update footer.js
    Adding disabling option

--- footer.js
+++ footer.js
@@ -1,13 +1,13 @@
-function makeFooter(t1, l1, t2, l2){
+function makeFooter(t1, l1, t2, l2, b1=false, b2=false){
 document.write(`
 <nav>
   <ul class="pagination justify-content-center">
     
-    <li class="page-item"><a class="page-link" href="${l1}">&laquo; ${t1}</a></li>
+    <li class="page-item ${b1? "disabled" : ""}"><a class="page-link" href="${l1}">&laquo; ${t1}</a></li>
     <li class="page-item active"><a class="page-link" href="">•</a></li>
-    <li class="page-item disabled"><a class="page-link" href="${l2}">${t2} &raquo;</a></li>
+    <li class="page-item ${b2? "disabled" : ""}"><a class="page-link" href="${l2}">${t2} &raquo;</a></li>
     
   </ul>
      </nav>
-`)
+`);
 }
