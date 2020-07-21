d3d450fa5cd192179025882f551718cfca936d89
Saumya Singhal <65318685+saumyasinghal747@users.noreply.github.com>
2020-07-21 11:04:53 -0700

    Add files via upload

--- search/search.js
+++ search/search.js
@@ -1,5 +1,5 @@
-function showR(){
-
+var sdata;
+function showR(sdata){
 var title;
 var desc ;
 var link ;
@@ -8,26 +8,35 @@ const cont =  document.getElementById("sres");
 const urlParams = new URLSearchParams(window.location.search);
 const myParam = urlParams.get('q');
 cont.innerHTML += `<h1>Search: ${myParam}</h1>`;
-var results = document.querySelectorAll(".gsc-webResult.gsc-result");
-var nResults = document.querySelectorAll(".gs-no-results-result");
-if (nResults.length == 1){
+var results = sdata.items
+var nResults = sdata.searchInformation.totalResults;
+if (nResults==0){
 document.getElementById("spin").style.display="none";
 cont.innerHTML += `<p class="mt-2">There are no results for your query.</p>`;
 cont.innerHTML += `<a href=#googleStuff class="text-muted">Search results and order determined by <u>Google CSE.</u></a>`;
 return null;}
 console.log(nResults.length);
 results.forEach(function (currentValue){
-title = currentValue.querySelector("a.gs-title").innerHTML;
-desc = currentValue.querySelector(".gs-bidi-start-align.gs-snippet").innerText;
-link = currentValue.querySelector(".gs-bidi-start-align.gs-visibleUrl.gs-visibleUrl-long").innerText;
+title = currentValue.htmlTitle;
+desc = currentValue.snippet;
+link = currentValue.formattedUrl;
 card = `<div class="card pop my-3 no-def">
 <div class="card-body">
     <h5 class="card-title">${title}</h5>
     <p class="card-text">${desc}</p>
-      <a href="${link}" class="float-right btn btn-lg btn-success stretched-link"  style="min-width:10rem">Visit</a>
+      <a href="${link}" class="float-right btn btn-lg text-light btn-success stretched-link"  style="min-width:10rem">Visit</a>
   </div>
 </div>`;
 cont.innerHTML += card;});
 cont.innerHTML += `<a href=#googleStuff class="text-muted">Search results and order determined by <u>Google CSE.</u></a>`;
 document.getElementById("spin").style.display="none";
+}
+
+function getData(){
+const urlParams = new URLSearchParams(window.location.search);
+const myParam = urlParams.get('q');
+fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyA6bA3PlHiFGHB1CIWobuBUsEa9IQ7AL3I&cx=016043130169791063080:mbkue25xulm&q=${myParam}`)
+.then(response => {return response.json()})
+.then(data => {console.log(data);showR(data)})
+return sdata;
 }
