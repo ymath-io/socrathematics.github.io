4cb84c15e5a967d4d6d41c311a39ee4fd10dc8c1
FrozenMango747 <realsaumya@gmail.com>
2021-02-09 06:23:52 +0530

    adding the link to discord

--- vue/pageFooter.js
+++ vue/pageFooter.js
@@ -11,12 +11,12 @@ Vue.component('page-footer', {
   <p class=""><img height="40px" class="mr-3" src="/faviconGrey.png"/><u><a class="text-dark" href="/faq/copyright-notice">© Copyright 2021 {{ alt==undefined? "Saumya Singhal" : "Sophia Zhu" }} - all rights reserved.</a></u></p> 
 </div>
 <div class="col text-right">
- <p class=""></p>
+ <p><a href="https://discord.gg/NPbfRt4wNM" tabindex="1" target="_blank" class="btn btn-link py-0  px-1"><img height="40px"  src="/icons/discordlogo.png"/> </a></p>
 </div>
 </div>
 </div>
-</footer></div>`
-});
+</footer>
+</div>`});
 
 var pageFoot = new Vue({
     el:".page-footer"
