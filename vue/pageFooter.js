9a25e62ff200c7d143983cb4db3c076ed3447f99
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-04-01 19:35:58 -0700

    YMotivate

--- vue/pageFooter.js
+++ vue/pageFooter.js
@@ -11,7 +11,7 @@ Vue.component('page-footer', {
   <p class=""><img height="40px" class="mr-3" src="/faviconGrey.png"/><u><a class="text-dark" href="/faq/copyright-notice">© Copyright 2021 {{ alt==undefined? "Saumya Singhal" : "Sophia Zhu" }} - all rights reserved.</a></u></p> 
 </div>
 <div class="col text-right">
- <p><a href="https://discord.gg/NPbfRt4wNM" tabindex="1" target="_blank" class="btn btn-link py-0  px-1"><img height="40px"  src="/icons/discordlogo.png"/> </a></p>
+ <p><a href="https://discord.gg/NPbfRt4wNM" tabindex="1" target="_blank" class="btn text-dark py-0  px-1"><img height="40px"  src="/icons/discordlogo.png"/> <u>Join us on Discord!</u></a></p>
 </div>
 </div>
 </div>
